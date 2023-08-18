7f6f12742dd3093a1d0a4f10b1a1f4d2e8a3e46f
Anahit-Fathollahi <45589564+Anahit-Fathollahi@users.noreply.github.com>
2023-08-18 19:24:38 +0430

    first-commit

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+const ErrorPage = () => {
+  return (
+    <div className="error_page">
+      <h1>404 - Page Not Found</h1>
+      <p>Sorry, there is nothing to see here</p>
+    </div>
+  );
+};
+
+export default ErrorPage;
